40af1607e700314fb62a3cd331fbc12e34a4f62b
Habid Enrique Manzur Restrepo <habidmanzur@outlook.com>
2025-10-24 15:40:15 -0500

    feat(core): fixed ring-mqtt error injecting vault values and added auo unseal in vault pod

--- vault-auth.sh
+++ vault-auth.sh
@@ -1,17 +1,7 @@
-TOKEN_REVIEWER_JWT=$(kubectl create token vault -n core)
-
-KUBE_HOST=$(kubectl get svc kubernetes -o jsonpath='{.spec.clusterIP}')
-
-KUBE_CA_CERT=$(kubectl get secret $(kubectl get sa vault-auth -n core -o jsonpath='{.secrets[0].name}') -n core -o jsonpath='{.data.ca\.crt}' | base64 --decode)
-
-# Si se cae la autenticación co vault ejecutar dentro de vault
 vault write auth/kubernetes/config \
-    token_reviewer_jwt="$TOKEN_REVIEWER_JWT" \
-    kubernetes_host="$KUBE_HOST" \
-    kubernetes_ca_cert="$KUBE_CA_CERT" \
-    disable_iss_validation=false \
-    issuer="https://kubernetes.default.svc.cluster.local"
-
+  token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+  kubernetes_host="https://kubernetes.default.svc.cluster.local:443" \
+  kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 
 vault read auth/kubernetes/config
 
