2b2d3868e59c5faf48cac09bc88cdf2dd2b99dfd
Habid Enrique Manzur Restrepo <habidmanzur@outlook.com>
2026-02-01 20:03:01 -0500

    feat: moving pods with nfs

--- scripts/vault-auth.sh
+++ scripts/vault-auth.sh
@@ -6,3 +6,17 @@ vault write auth/kubernetes/config \
 vault read auth/kubernetes/config
 
 vault write auth/kubernetes/login role=vault jwt="$TOKEN_REVIEWER_JWT"
+
+vault write auth/jwt/role/github-ansible-role - <<EOF
+{
+  "role_type": "jwt",
+  "user_claim": "actor",
+  "bound_claims_type": "glob",
+  "bound_claims": {
+    "repository": "citocafecito-homelab/*"
+  },
+  "bound_audiences": "https://vault.core.svc.cluster.local",
+  "policies": ["citocafecito-readonly", "ssh-signer"],
+  "ttl": "10m"
+}
+EOF
